a0e37531246f8c46ae9dd4069aed2d31c265f80d
Tanya <85761740+Codr786@users.noreply.github.com>
2024-01-04 22:09:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    canvas.position(570,200);
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('myFILTERImage.png')
+}
